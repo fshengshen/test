697efd48b7b30012ec8747d5dbc63fc23a1a78ef
fangyinan <fyn@aek56.com>
2017-05-19 15:03:48 +0800

    read

--- src/main/java/com/shensheng/Application.java
+++ src/main/java/com/shensheng/Application.java
@@ -2,22 +2,13 @@ package com.shensheng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by shensheng on 2017/3/27.
  */
 
 @SpringBootApplication
-@RestController
 public class Application {
-
-    @GetMapping(value = "/index")
-    public String greeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
