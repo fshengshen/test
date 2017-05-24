8130f3b979dacf94a23d2fc3ed3b293fd2d7a43a
fangyinan <fyn@aek56.com>
2017-05-24 11:35:59 +0800

    read

--- src/main/java/com/shensheng/service/impl/TestServiceImpl.java
+++ src/main/java/com/shensheng/service/impl/TestServiceImpl.java
@@ -3,6 +3,8 @@ package com.shensheng.service.impl;
 import com.shensheng.persistence.beans.User;
 import com.shensheng.persistence.mapper.UserInfoMapper;
 import com.shensheng.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +17,15 @@ public class TestServiceImpl implements TestService {
     @Autowired
     private UserInfoMapper userInfoMapper;
 
+    private Logger logger = LoggerFactory.getLogger(TestServiceImpl.class);
+
     @Override
     public User login() {
         User user = new User();
         user.setUserName("1");
         user.setPassword("1");
         user = userInfoMapper.check(user);
+        logger.debug("ttt");
         return user;
     }
 }
